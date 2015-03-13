f8e2bf6a92fafbaa2b76c8f03dbd2861ca12df5d
Derek S <dereksilverman@gmail.com>
2015-03-13 05:51:02 -0400

    Mifugo Trade

--- app/mailers/submission.rb
+++ app/mailers/submission.rb
@@ -1,16 +1,16 @@
 class Submission < ActionMailer::Base
-  default from: "Rangeland Trading <auction@rangelandtrading.co.ke>"
+  default from: "Mifugo Trade <auction@rangelandtrading.co.ke>"
   def livestock_submission(livestock)
     @livestock = livestock
     mail(to: livestock.phone,
-         subject: "[Rangeland Trading Submission] Thanks #{livestock.seller} (#{livestock.phone})"
+         subject: "[Mifugo Trade Submission] Thanks #{livestock.seller} (#{livestock.phone})"
          )
   end
 
   def admin_livestock_submission(livestock)
     @livestock = livestock
     mail(to: "tgtully@gmail.com",
-         subject: "[Rangeland Trading Submission] Thanks #{livestock.seller} (#{livestock.phone})"
+         subject: "[Mifugo Trade Submission] Thanks #{livestock.seller} (#{livestock.phone})"
          )
   end
 
